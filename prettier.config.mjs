dff45a4715bf1573ec3537ad2e5ad30132f20a48
Carlos Cosming <carlos@dynamicquants.com>
2025-05-03 23:47:17 -0400

    chore(config): update import order in prettier configuration
    - Modified the import order in the Prettier configuration to prioritize imports from the @daikit package, followed by relative imports. This change enhances code organization and consistency in import statements.

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,5 +2,5 @@ import prettierConfig from '@unokit/prettier';
 
 export default prettierConfig({
   plugins: ['sortImports', 'multilineArrays'],
-  importOrder: ['^[./]'],
+  importOrder: ['^@daikit/(.*)$', '^[./]'],
 });
