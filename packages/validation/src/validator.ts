bbb32fba7e560e99dd7e1dea328943015f4d3a15
Carlos Cosming <carlos@dynamicquants.com>
2025-05-09 21:18:24 -0400

    refact(validation): remove unused types from validator.ts
    - Eliminated unnecessary types and comments from the validator.ts file, streamlining the code and improving clarity. This includes the removal of ValidationAny, ValidatorList, ValidatorMap, and associated error inference types.

--- packages/validation/src/validator.ts
+++ packages/validation/src/validator.ts
@@ -9,15 +9,6 @@
 import { type Result } from '@daikit/result';
 
 /**
- * A type that is used to justify the use of any in validation contexts.
- * It is used to avoid type errors when using the any type through the codebase.
- *
- * @internal
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ValidationAny = any;
-
-/**
  * Represents a validator function that takes an input and returns a Result.
  *
  * @typeParam T - The type of the input value.
@@ -29,50 +20,4 @@ type ValidationAny = any;
  */
 type Validator<T, E extends string> = (input: T) => Result<void, E>;
 
-/**
- * Represents a list of validators.
- *
- * @typeParam T - The type of the input value.
- *
- * @public
- */
-type ValidatorList<T> = readonly Validator<T, string>[];
-
-/**
- * Represents a map of field validators.
- *
- * @typeParam T - The type of the input value.
- *
- * @public
- */
-type ValidatorMap<T> = {
-  [K in keyof T]?: ValidatorList<T[K]>;
-};
-
-/**
- * Infer the error type from a list of validators.
- *
- * @typeParam V - The type of the list of validators.
- * @returns The error type.
- *
- * @public
- */
-type ValidatorListErrors<V extends readonly Validator<ValidationAny, ValidationAny>[]> =
-  V[number] extends Validator<ValidationAny, infer E> ? E : never;
-
-/**
- * Infer the error type from a field validators map.
- *
- * @typeParam FV - The type of the field validators map.
- * @returns The error type.
- *
- * @public
- */
-type ValidatorMapErrors<T> =
-  T extends ValidatorMap<ValidationAny>
-    ? {
-        [K in keyof T]: T[K] extends readonly Validator<ValidationAny, infer E>[] ? E : never;
-      }[keyof T]
-    : never;
-
-export type { ValidatorMapErrors, ValidatorListErrors, Validator, ValidatorList, ValidatorMap };
+export type { Validator };
