3ace92adfdd4dabd2490743c310c8533d89f33cd
Carlos Cosming <carlos@dynamicquants.com>
2025-05-18 23:45:55 -0400

    feat(core): add Any and Prettify types for improved type handling
    - Introduced `Any` type to represent justified `any` values, promoting better type safety across the codebase.
    - Added `Prettify` type to enhance readability of complex types, making them easier to work with.
    - Updated index file to export new types, ensuring they are accessible for use in other modules.

---
+++ packages/core/src/types/prettify.ts
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the Daikit project.
+ *
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Prettify is a type that converts a messy complex/composed types into a more readable format.
+ *
+ * @typeParam T - The type to prettify.
+ * @returns The prettified type.
+ *
+ * @public
+ */
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+export type { Prettify };
