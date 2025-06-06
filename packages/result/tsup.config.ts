f3f24a61662bdf5fa7e7060ad379137f6301e5a8
Carlos Cosming <carlos@dynamicquants.com>
2025-06-06 12:30:53 -0400

    chore: remove deprecated result package

--- packages/result/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import tsupConfig from '@unokit/tsup';
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  ...tsupConfig,
-  entry: ['src/index.ts'],
-  tsconfig: 'tsconfig.lib.json',
-});
