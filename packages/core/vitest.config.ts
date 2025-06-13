f72a90d722fc04cf36dde23cba0ce21f056af937
Carlos Cosming <carlos@dynamicquants.com>
2025-06-12 23:52:05 -0400

    chore(core): update vitest configuration and add sideEffects flag
    - Added a `sideEffects` flag to the package.json to optimize tree-shaking.
    - Refactored vitest configuration to define multiple test projects with type checking for improved test organization and clarity.

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,1 +1,48 @@
-export { vitestBase as default } from '@unokit/vitest';
+import { defineVitestConfig } from '@unokit/vitest';
+
+export default defineVitestConfig({
+  projects: {
+    mode: 'replace',
+    config: [
+      {
+        test: {
+          name: 'pipe',
+          include: ['tests/pipe.spec.ts'],
+          typecheck: {
+            enabled: true,
+            include: ['tests/pipe.spec.ts'],
+            checker: 'tsc',
+          },
+        },
+      },
+      {
+        test: {
+          name: 'flow',
+          include: ['tests/flow.spec.ts'],
+          typecheck: {
+            enabled: true,
+            include: ['tests/flow.spec.ts'],
+            checker: 'tsc',
+          },
+        },
+      },
+      {
+        test: {
+          name: 'result',
+          include: ['tests/result.spec.ts'],
+          typecheck: {
+            enabled: true,
+            include: ['tests/result.spec.ts'],
+            checker: 'tsc',
+          },
+        },
+      },
+      {
+        test: {
+          name: 'metadata',
+          include: ['tests/metadata.spec.ts'],
+        },
+      },
+    ],
+  },
+});
