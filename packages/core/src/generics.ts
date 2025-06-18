552ad0a35336551c0335351743119f9b43d7c85a
Carlos Cosming <carlos@dynamicquants.com>
2025-06-17 20:14:53 -0400

    feat(core): add generics utility type `Any`
    - Introduced a new file `generics.ts` containing the `Any` type to represent justified `any` values throughout the codebase, promoting better type safety and avoiding explicit `any` usage.
    - Included documentation for the `Any` type to clarify its purpose and usage within the project.

---
+++ packages/core/src/generics.ts
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the Daikit project.
+ *
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// *********************************************************************************************
+// General purpose types.
+// *********************************************************************************************
+
+/**
+ * Any is used to reflect justified `any` values through the codebase and avoid use the explicit
+ * `any` type.
+ *
+ * @public
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Any = any;
+
+export type { Any };
