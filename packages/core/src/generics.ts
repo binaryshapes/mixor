bb3094bbcfff903e3f9708d442eb04e63f38ec52
Carlos Cosming <carlos@dynamicquants.com>
2025-06-21 23:57:37 -0400

    feat(core): enhance generics with PrimitiveType and Prettify utilities
    - Added `PrimitiveType` and `PrimitiveTypeExtended` types to represent primitive and extended types, respectively.
    - Introduced `Prettify` type for converting complex types into more readable formats, with detailed type handling for arrays and objects.
    - Included comprehensive documentation for new types to improve clarity and usability within the codebase.

--- packages/core/src/generics.ts
+++ packages/core/src/generics.ts
@@ -20,4 +20,39 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Any = any;
 
-export type { Any };
+/**
+ * PrimitiveType is a type that represents all the primitive types.
+ *
+ * @public
+ */
+type PrimitiveType = string | number | boolean | null | undefined | bigint | symbol;
+
+/**
+ * PrimitiveTypeExtended is a type that represents all the primitive types and some other types
+ * that are not primitive but are considered primitive in some scenarios.
+ *
+ * @public
+ */
+type PrimitiveTypeExtended = PrimitiveType | Date | RegExp | Array<Any>;
+
+/**
+ * Prettify is a type that converts a messy complex/composed types into a more readable format.
+ *
+ * @typeParam T - The type to prettify.
+ * @returns The prettified type.
+ *
+ * @public
+ */
+type Prettify<T> = T extends PrimitiveType
+  ? T
+  : {} & T extends Array<infer U>
+    ? // TODO: Check if this is correct for arrays. We have some cases where the array is
+      // changed. Ej: [string, string, number] -> (string | number)[]. See more in pipe helpers.
+      Array<Prettify<U>>
+    : T extends object
+      ? {
+          [K in keyof T]: T[K] extends PrimitiveType ? Prettify<T[K]> : T[K];
+        }
+      : T;
+
+export type { Any, Prettify, PrimitiveType, PrimitiveTypeExtended };
