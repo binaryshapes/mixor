47bdaf16a62d9a9051d5be23bedebdc8a824f4b1
Carlos Cosming <carlos@dynamicquants.com>
2025-06-26 23:50:12 -0400

    chore(core): update vitest project configs

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -17,6 +17,28 @@ export default defineVitestConfig({
       },
       {
         test: {
+          name: 'builder',
+          include: ['tests/builder.spec.ts'],
+          typecheck: {
+            enabled: true,
+            include: ['tests/builder.spec.ts'],
+            checker: 'tsc',
+          },
+        },
+      },
+      {
+        test: {
+          name: 'flow2',
+          include: ['tests/flow2.spec.ts'],
+          typecheck: {
+            enabled: true,
+            include: ['tests/flow2.spec.ts'],
+            checker: 'tsc',
+          },
+        },
+      },
+      {
+        test: {
           name: 'flow',
           include: ['tests/flow.spec.ts'],
           typecheck: {
@@ -39,8 +61,30 @@ export default defineVitestConfig({
       },
       {
         test: {
+          name: 'panic',
+          include: ['tests/panic.spec.ts'],
+          typecheck: {
+            enabled: true,
+            include: ['tests/panic.spec.ts'],
+            checker: 'tsc',
+          },
+        },
+      },
+      {
+        test: {
           name: 'metadata',
           include: ['tests/metadata.spec.ts'],
+        },
+      },
+      {
+        test: {
+          name: 'container',
+          include: ['tests/container.spec.ts'],
+          typecheck: {
+            enabled: true,
+            include: ['tests/container.spec.ts'],
+            checker: 'tsc',
+          },
         },
       },
     ],
