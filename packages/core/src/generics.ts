c513a533d0842c77ed43df52f13cb7b41a6d5633
Carlos Cosming <carlos@dynamicquants.com>
2025-07-03 17:20:41 -0400

    feat(core): add utility types for promises and object manipulation
    - Added `IsPromise`, `HasPromise`, `DeepAwaited`, `IsPrimitive`, `UnionKeys`, and `MergeUnion` types to enhance type safety and utility in handling Promises and object manipulations.
    - Updated the `Prettify` type to include support for `never`, improving type inference.
    - Enhanced documentation for new types, providing clear usage examples and descriptions for better developer understanding.

--- packages/core/src/generics.ts
+++ packages/core/src/generics.ts
@@ -43,7 +43,7 @@ type PrimitiveTypeExtended = PrimitiveType | Date | RegExp | Array<Any>;
  *
  * @public
  */
-type Prettify<T> = T extends PrimitiveType
+type Prettify<T> = T extends PrimitiveType | never
   ? T
   : {} & T extends Array<infer U>
     ? // TODO: Check if this is correct for arrays. We have some cases where the array is
@@ -55,4 +55,153 @@ type Prettify<T> = T extends PrimitiveType
         }
       : T;
 
-export type { Any, Prettify, PrimitiveType, PrimitiveTypeExtended };
+/**
+ * Helper type to detect if a type is a Promise
+ *
+ * @typeParam T - The type to check
+ * @returns True if T is a Promise
+ *
+ * @public
+ */
+type IsPromise<T> = T extends Promise<unknown> ? true : false;
+
+/**
+ * Inspect if any of the sub-types are promises.
+ *
+ * @typeParam T - The type to inspect.
+ * @returns True if any of the sub-types are promises, false otherwise.
+ *
+ * @public
+ */
+type HasPromise<T> =
+  T extends Promise<unknown>
+    ? true
+    : T extends Array<infer U>
+      ? HasPromise<U>
+      : T extends object
+        ? {
+            [K in keyof T]: IsPromise<T[K]> extends true
+              ? true
+              : T[K] extends object
+                ? HasPromise<T[K]>
+                : false;
+          }[keyof T] extends false
+          ? false
+          : true
+        : false;
+
+/**
+ * Recursively unwraps all Promise types in a type, including nested objects and arrays.
+ *
+ * @typeParam T - The type to unwrap Promises from
+ * @typeParam O - The type of the object to avoid unwrapping.
+ *
+ * @example
+ * ```ts
+ * type User = {
+ *   id: number;
+ *   name: string;
+ *   nested: {
+ *     email: Promise<string>;
+ *   };
+ *   tags: Promise<string[]>;
+ * };
+ *
+ * type ResolvedUser = DeepAwaited<User>;
+ * // Result:
+ * // {
+ * //   id: number;
+ * //   name: string;
+ * //   nested: {
+ * //     email: string;
+ * //   };
+ * //   tags: string[];
+ * // }
+ * ```
+ *
+ * @public
+ */
+type DeepAwaited<T, O extends Record<string, Any> = never> =
+  T extends Promise<infer U>
+    ? DeepAwaited<U, O>
+    : T extends Array<infer A>
+      ? Array<DeepAwaited<A>>
+      : T extends object
+        ? T extends O
+          ? T
+          : {
+              [K in keyof T]: DeepAwaited<T[K], O>;
+            }
+        : T;
+
+/**
+ * Checks if a type is a primitive type.
+ *
+ * @typeParam T - The type to check.
+ * @returns True if the type is a primitive type, false otherwise.
+ *
+ * @public
+ */
+type IsPrimitive<T> = T extends object ? false : true;
+
+// *********************************************************************************************
+// Object manipulation types.
+// *********************************************************************************************
+
+/**
+ * Gets all the keys of a union type.
+ *
+ * @typeParam T - The type to get the keys from.
+ * @returns The keys of the union type.
+ *
+ * @public
+ */
+type UnionKeys<T> = T extends T ? keyof T : never;
+
+/**
+ * Merges a union type into a single type.
+ *
+ * @typeParam T - The type to merge.
+ * @returns The merged type.
+ *
+ * @example
+ * ```ts
+ * type User = {
+ *   name: string;
+ *   category: 'admin';
+ * } | {
+ *   name: string;
+ *   category:  'user';
+ * };
+ *
+ * type UserMerged = MergeUnion<User>;
+ * // Result:
+ * // {
+ * //   name: string;
+ * //   category: 'admin' | 'user';
+ * // }
+ * ```
+ *
+ * @public
+ */
+type MergeUnion<T> =
+  IsPrimitive<T> extends true
+    ? T
+    : {
+        [K in UnionKeys<T>]: T extends Any ? (K extends keyof T ? T[K] : never) : never;
+      };
+
+export type {
+  // General purpose types.
+  Any,
+  Prettify,
+  PrimitiveType,
+  PrimitiveTypeExtended,
+  HasPromise,
+  IsPromise,
+  DeepAwaited,
+  IsPrimitive,
+  // Object manipulation types.
+  UnionKeys,
+  MergeUnion,
+};
