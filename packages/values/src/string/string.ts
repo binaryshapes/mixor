304d37cf6283c0e33b8e107a96595e062dd7c536
Carlos Cosming <carlos@dynamicquants.com>
2025-08-13 23:00:22 -0400

    feat(values): introduce StringValueError type for string validation
    - Added a new error type, StringValueError, to represent validation errors for string values.
    - This type extends the ResultError, providing a structured way to handle string-specific errors, enhancing clarity in error management.

--- packages/values/src/string/string.ts
+++ packages/values/src/string/string.ts
@@ -1,4 +1,15 @@
-import { type Any, type Rule, value } from '@mixor/core';
+import { type Any, type ResultError, type Rule, value } from '@mixor/core';
+
+/**
+ * Result base error type for string values rules.
+ *
+ * @remarks
+ * This error type is used to represent the error that occurs when a string value is not valid.
+ * It is a base error type for all string value errors.
+ *
+ * @public
+ */
+type StringValueError<C extends string, O extends string> = ResultError<C, 'StringValue', O>;
 
 /**
  * Creates a string value with automatic type inference for tracing.
@@ -10,4 +21,5 @@ import { type Any, type Rule, value } from '@mixor/core';
  */
 const string = <T extends Rule<string, Any>[]>(...rules: T) => value<T>(...rules).subType('string');
 
+export type { StringValueError };
 export { string };
