74870cf655b767468676f66fe530a2725cc9348e
Carlos Cosming <carlos@dynamicquants.com>
2025-08-13 23:03:20 -0400

    feat(values): refactor isString validation and error handling
    - Simplified the `isString` function implementation for better readability.
    - Updated the error handling by introducing a new `NotString` error type, enhancing clarity in validation failures.
    - Removed redundant examples from the documentation to streamline the codebase.

--- packages/values/src/string/is-string.ts
+++ packages/values/src/string/is-string.ts
@@ -1,81 +1,34 @@
-import { type Result, type ResultError, err, ok, rule } from '@mixor/core';
+import { err, ok, rule } from '@mixor/core';
+
+import type { StringValueError } from './string';
 
 /**
- * Result error type for the `isString` rule.
+ * Result error type related to the string `isString` rule.
  *
- * @remarks
- * This error type is used to represent the error that occurs when the value is not a string.
- *
- * @public
+ * @internal
  */
-type NotString = ResultError<'NOT_STRING', 'isString'>;
+type NotString = StringValueError<'NotStringError', 'isString'>;
 
 /**
- * Validates that the value is a string.
+ * Instance of the `NotString` error type.
+ *
+ * @internal
+ */
+const NotString: NotString = {
+  code: 'NotStringError',
+  context: 'StringValue',
+  origin: 'isString',
+  message: 'Value is not a string',
+};
+
+/**
+ * Value rule that validates that the value is a string.
  *
  * @param value - The unknown value to validate.
  * @returns A result indicating whether the value is a string.
  *
- * @example
- * ```ts
- * // is-string-001: Validate string value.
- * const result = isString('hello');
- * // result: ok('hello').
- * ```
- *
- * @example
- * ```ts
- * // is-string-002: Reject number value.
- * const result = isString(1.23);
- * // result: err(NotString).
- * ```
- *
- * @example
- * ```ts
- * // is-string-003: Reject boolean value.
- * const result = isString(true);
- * // result: err(NotString).
- * ```
- *
- * @example
- * ```ts
- * // is-string-004: Reject null value.
- * const result = isString(null);
- * // result: err(NotString).
- * ```
- *
- * @example
- * ```ts
- * // is-string-005: Reject undefined value.
- * const result = isString(undefined);
- * // result: err(NotString).
- * ```
- *
- * @example
- * ```ts
- * // is-string-006: Reject object value.
- * const result = isString({});
- * // result: err(NotString).
- * ```
- *
- * @example
- * ```ts
- * // is-string-007: Reject array value.
- * const result = isString([]);
- * // result: err(NotString).
- * ```
- *
  * @public
  */
-const isString = rule(
-  (value: unknown): Result<string, NotString> =>
-    typeof value === 'string'
-      ? ok(value)
-      : err({
-          code: 'NOT_STRING',
-          context: 'isString',
-          message: `${value} is not string`,
-        }),
-);
+const isString = rule((value: unknown) => (typeof value === 'string' ? ok(value) : err(NotString)));
 
 export { isString };
