e02bd87f0c0b784da836fed73b1b4afe0ecc12da
Carlos Cosming <carlos@dynamicquants.com>
2025-08-13 22:49:29 -0400

    feat(values): refactor string value creation for type inference
    - Updated the string value creation function to simplify its implementation and enhance type inference.
    - Removed the traceable wrapper and replaced it with a more straightforward value function, improving clarity and maintainability.

--- packages/values/src/string/string.ts
+++ packages/values/src/string/string.ts
@@ -1,19 +1,13 @@
-import { type ErrorMode, type Value, type ValueRule, pipe, traceable } from '@mixor/core';
+import { type Any, type Rule, value } from '@mixor/core';
 
 /**
  * Creates a string value with automatic type inference for tracing.
  *
  * @param rules - The validation rules to apply to the string value.
- * @returns A new traceable string value.
+ * @returns A new string value.
  *
  * @public
  */
-const string = <E>(...rules: ValueRule<string, E>[]) =>
-  traceable(
-    'Value',
-    (value: string, mode: ErrorMode = 'all') =>
-      pipe(mode, ...(rules as [ValueRule<string, E[]>]))(value),
-    'string',
-  ) as Value<string, E>;
+const string = <T extends Rule<string, Any>[]>(...rules: T) => value<T>(...rules).subType('string');
 
 export { string };
