fdffa02674ef9e8fa9a089f25b6fe3e14c1fe205
Carlos Cosming <carlos@kingly.cl>
2025-09-06 20:42:43 -0400

    🔧 chore(core): remove unused struct export from schema index
    - Eliminated the export of the `struct` module from the schema index file to streamline the codebase and improve maintainability.

--- packages/core/src/schema/index.ts
+++ packages/core/src/schema/index.ts
@@ -5,7 +5,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 export * from './rule';
 export * from './schema';
-export * from './struct';
 export * from './value';
