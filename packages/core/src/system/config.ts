a9df8e74ff84c6f6a958c16d529a83df43bccd8b
Carlos Cosming <carlos@dynamicquants.com>
2025-09-08 12:07:57 -0300

    feat(core): add core configuration and types for error handling
    - Introduced a new `config.ts` file containing the core internal configuration, including a default error mode for values and schemas.
    - Added a `types.ts` file defining the `ErrorMode` type and a helper type `ApplyErrorMode` for applying error modes to error types.
    - Updated the `index.ts` file to export the new configuration and types, enhancing the organization of the core system module.
    - This addition aims to improve error handling and configuration management within the Nuxo project.

---
+++ packages/core/src/system/config.ts
@@ -0,0 +1,28 @@
+/**
+ * This file is part of the Nuxo project.
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import type { ErrorMode } from './types';
+
+/**
+ * Core internal configuration.
+ *
+ * @public
+ */
+const Config = {
+  /**
+   * Default error mode for values and schemas.
+   *
+   * @remarks
+   * - `all`: The schema will return all errors (default).
+   * - `strict`: The schema will return the first error.
+   *
+   * @see {@link ErrorMode}
+   */
+  defaultErrorMode: 'all' as ErrorMode,
+};
+
+export { Config };
