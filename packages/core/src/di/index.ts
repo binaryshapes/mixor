90820974e3f4f2b5476ad4713c24999e5a97802d
Carlos Cosming <carlos@dynamicquants.com>
2025-10-01 12:34:57 -0300

    feat(core): implement container module for dependency management
    - Introduced a new `container.ts` file to implement a container system for managing dependencies within the Nuxo project.
    - Defined types and classes for building a container, including `ContainerBuilder`, `ContainerError`, and utility functions for adding and resolving ports and services.
    - Enhanced the architecture by providing a structured way to handle service dependencies and port bindings, improving maintainability and usability.

--- packages/core/src/di/index.ts
+++ packages/core/src/di/index.ts
@@ -6,5 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 export * from './adapter';
+export * from './container';
 export * from './contract';
 export * from './port';
+export * from './service';
