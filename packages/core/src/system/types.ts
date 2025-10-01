88a39620a9f53297d3223c2e4a1d5f6199215d59
Carlos Cosming <carlos@dynamicquants.com>
2025-10-01 12:30:49 -0300

    feat(core): add TypeOf utility generic
    - Introduced a new `TypeOf` type to extract the type of a component based on its structure, enhancing type safety and usability within the Nuxo project.
    - This addition aims to improve type inference and clarity when working with component types.

--- packages/core/src/system/types.ts
+++ packages/core/src/system/types.ts
@@ -5,6 +5,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import type { Any, FlatArray } from '../utils';
 
 /**
@@ -26,5 +27,17 @@ type ErrorMode = 'strict' | 'all';
 type ApplyErrorMode<E, Mode extends ErrorMode, FE = FlatArray<E>> = Mode extends 'strict'
   ? Exclude<FE, Array<Any>>
   : FE[];
+
+/**
+ * Extracts the type of a component.
+ *
+ * @typeParam T - The component to extract the type from.
+ * @returns The type of the component.
+ *
+ * @public
+ */
+type TypeOf<T> = T extends { Type: infer U; Tag: string } ? U : never;
+
+export type { TypeOf };
 
 export type { ApplyErrorMode, ErrorMode };
