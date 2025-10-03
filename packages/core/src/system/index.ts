35a9bb43821e1aac6f81b0bc7fff2ede7185fee3
Carlos Cosming <carlos@dynamicquants.com>
2025-10-03 11:24:39 -0300

    feat(core): add global cache for resolved dependencies
    - Introduced a new `cache.ts` file to implement a global cache for storing resolved dependencies related to containers, services, adapters, and ports.
    - Updated the `index.ts` file to export the cache, enhancing the modularity and accessibility of the dependency management system within the Nuxo project.

--- packages/core/src/system/index.ts
+++ packages/core/src/system/index.ts
@@ -5,6 +5,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+export * from './cache';
 export * from './component';
 export * from './config';
 export * from './logger';
