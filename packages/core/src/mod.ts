f83e9d4412ba2a9dacf3bebf333e1fb0239fba4d
Carlos Cosming <carlos@kingly.cl>
2025-10-20 12:04:13 -0300

    ✨ feat(core): create core package using Deno
    - Introduced a new `deno.json` file to replace the existing `package.json` and `tsconfig.json`, streamlining the configuration for Deno.
    - Removed obsolete configuration files, including `package.json`, `tsconfig.json`, and related TypeScript configuration files, to simplify the project structure.
    - The new `deno.json` includes essential project metadata, tasks for development, building, formatting, and testing, enhancing the development workflow.

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -1,10 +1,7 @@
-/**
+/*
  * This file is part of the Nuxo project.
  * Copyright (c) 2025, Binary Shapes.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export * from './assert';
-export * from './failure';
-export * from './result';
