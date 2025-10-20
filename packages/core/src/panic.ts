ba7e11a8108ce2f207a521bd626ac1045b9aafb9
Carlos Cosming <carlos@kingly.cl>
2025-10-20 19:23:10 -0300

    ✨ feat(core): add panic module for handling unrecoverable runtime errors
    - Introduced a new `panic.ts` file that defines the `PanicError` class to represent non-recoverable runtime errors, enhancing error signaling in the application.
    - Added a factory function `panic` to create panic errors with specific scopes, ensuring type safety and consistency in error management.
    - Integrated logging functionality to capture error messages, improving debugging and error tracking capabilities.

---
+++ packages/core/src/panic.ts
@@ -0,0 +1,78 @@
+/**
+ * This file is part of the Nuxo project.
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { logger } from './logger.ts';
+
+/**
+ * A panic represents a runtime error that is not recoverable.
+ * It is used to signal that the application is in an invalid state and should be terminated.
+ * All core components should throw a panic error when they are in an invalid state.
+ *
+ * @param S - The scope of the panic.
+ * @param T - The tag of the panic.
+ *
+ * @internal
+ */
+class PanicError<S extends string, T extends string> extends Error {
+  /**
+   * Unique code of the panic error.
+   *
+   * @remarks
+   * This a convenient way to identify the panic error origin, for instance in error handling
+   * with pattern matching.
+   */
+  public readonly code: `${S}.${T}`;
+
+  /**
+   * Hint of the panic error.
+   *
+   * @remarks
+   * This is a hint to help the user understand the panic error.
+   */
+  public readonly hint?: string;
+
+  constructor(scope: S, tag: T, message: string, hint?: string) {
+    const errorMessage = `${message} ${hint ? `\n${hint}` : ''}`;
+    logger.error(errorMessage);
+    super(errorMessage);
+    this.code = `${scope}.${tag}`;
+    this.hint = hint;
+  }
+
+  /**
+   * Returns a JSON representation of the panic error.
+   * @returns A JSON representation of the panic error.
+   */
+  public toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      code: this.code,
+    };
+  }
+}
+
+/**
+ * Factory function to create a panic error for the given scope.
+ *
+ * @remarks
+ * Useful to create panic errors with a specific scope ensuring type safety and consistency.
+ *
+ * @param scope - The scope of the panic.
+ * @returns A panic error class for the given scope.
+ *
+ * @public
+ */
+const panic = <S extends string, T extends string>(scope: S) =>
+  class extends PanicError<S, T> {
+    constructor(tag: T, message: string, hint?: string) {
+      super(scope, tag, message, hint);
+    }
+  };
+
+export { panic };
