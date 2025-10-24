84f3dce326edb144901e3bc573ae44215aa70254
Carlos Cosming <carlos@kingly.cl>
2025-10-24 17:56:31 -0300

    ✨ feat(core): add core configuration module
    - Introduced a new `config.ts` file that defines core configuration variables and a configuration manager.
    - The configuration manager provides `get` and `set` methods for managing configuration values, enhancing flexibility and usability in the application.

---
+++ packages/core/src/config.ts
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the Nuxo project.
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Core configuration variables.
+ *
+ * @internal
+ */
+const vars = {
+  /**
+   * The filename for the exported registry.
+   */
+  REGISTRY_FILENAME: 'nuxo-registry.json',
+};
+
+/**
+ * Core configuration manager.
+ *
+ * @public
+ */
+const config = {
+  /**
+   * Gets a configuration value by key.
+   *
+   * @param key - The key of the configuration value to get.
+   * @returns The configuration value.
+   */
+  get: <T extends keyof typeof vars>(key: T) => vars[key],
+
+  /**
+   * Sets a configuration value by key.
+   *
+   * @param key - The key of the configuration value to set.
+   * @param value - The value to set.
+   */
+  set: <T extends keyof typeof vars>(key: T, value: typeof vars[T]) => (vars[key] = value),
+};
+
+export { config };
