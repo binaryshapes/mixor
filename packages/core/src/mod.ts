2778404a5eb91173c29751c59087a78fd04e0c72
Carlos Cosming <carlos@dynamicquants.com>
2025-10-25 00:12:21 -0300

    ✨ feat(core): add core module and enhance exports
    - Created a new `mod.ts` file to serve as the main entry point for the core package, facilitating easier imports.
    - Updated `src/mod.ts` to export all relevant modules, improving accessibility and organization of core functionalities.

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -5,3 +5,17 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
+export * from './component.ts';
+export * from './config.ts';
+export * from './data.ts';
+export * from './flow.ts';
+export * from './logger.ts';
+export * from './panic.ts';
+export * from './pipe.ts';
+export * from './registry.ts';
+export * from './result.ts';
+export * from './utils.ts';
+
+export type * from './generics.ts';
+export type * from './types.ts';
