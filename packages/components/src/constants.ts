9d4c41215cbf64a3847451bdacb410c541a0fd08
Carlos Cosming <carlos@dynamicquants.com>
2025-10-25 15:12:19 -0300

    ✨ feat(components): add constants and types
    - Introduced a new `constants.ts` file defining the default error mode for validation.
    - Added a new `types.ts` file with a utility type `TypeOf` for extracting component types, enhancing type safety and usability in the components package.

---
+++ packages/components/src/constants.ts
@@ -0,0 +1,18 @@
+/**
+ * This file is part of the Nuxo project.
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import type { n } from '@nuxo/core';
+
+/**
+ * The default error mode for validation.
+ *
+ * @public
+ */
+const DEFAULT_ERROR_MODE: n.ErrorMode = 'all';
+
+export { DEFAULT_ERROR_MODE };
