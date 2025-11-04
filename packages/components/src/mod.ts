73f850f93efca26931962fc94a47c7ebd442d936
Carlos Cosming <carlos@dynamicquants.com>
2025-11-04 17:58:43 -0300

    feat(components): add mod.ts for package exports
    - Introduced a new `mod.ts` file to centralize exports for `rule.ts`, `schema.ts`, and `value.ts`, improving module organization and accessibility within the components package.
    - This addition enhances the structure of the components module, facilitating easier imports for users.

---
+++ packages/components/src/mod.ts
@@ -0,0 +1,11 @@
+/**
+ * This file is part of the Nuxo project.
+ * Copyright (c) 2025, Binary Shapes.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export * from './rule.ts';
+export * from './schema.ts';
+export * from './value.ts';
