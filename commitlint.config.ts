1de450838e8743f067d0af1e9f674fa76418d52e
Carlos Cosming <carlos@dynamicquants.com>
2025-11-05 10:49:55 -0300

    chore: update commitlint configuration and GitHub Actions workflow
    - Simplified the commitlint configuration by extending from `@commitlint/config-conventional` and removing custom rules.
    - Enhanced the GitHub Actions workflow to include a filtering job that checks commit messages for release-worthiness before proceeding with validation and release steps.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,42 +1,23 @@
 import type { UserConfig } from '@commitlint/types';
 
 const Config: UserConfig = {
-  parserPreset: {
-    parserOpts: {
-      headerPattern:
-        /^(?<type>.*\s\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))$/,
-      headerCorrespondence: ['type', 'scope', 'subject'],
-    },
-  },
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [2, 'always'],
-    'header-max-length': [2, 'always', 72],
-    'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
-    'subject-empty': [2, 'never'],
-    'subject-exclamation-mark': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
-    'type-empty': [2, 'never'],
+    'body-max-line-length': [0], // Disable body max line length check.
     'type-enum': [
       2,
       'always',
       [
-        '✨ feat', // Add new feature
-        '🐛 fix', // Solve a bug
-        '📚 docs', // Add or alter documentation
-        '💄 style', // Improve formatting, white-space
-        '🚀 perf', // Improve performance
-        '🧪 test', // Add or modify tests
-        '♻️ refact', // Rewrites code without feature, performance or bug changes
-        '⏪ revert', // Changes that reverting other changes
-        '🔀 merge', // Merge branches
-        '🔧 chore', // Changes to the build process or auxiliary tools and libraries such as documentation generation
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'perf',
+        'test',
+        'refact',
+        'revert',
+        'merge',
+        'chore',
       ],
     ],
   },
