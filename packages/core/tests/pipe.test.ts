7e3cc5b3f0969d9f40e3de504d8affabd1c91a21
Carlos Cosming <carlos@dynamicquants.com>
2025-12-24 00:15:11 -0300

    test(core/pipe): add failure class to pipe test

--- packages/core/tests/pipe.test.ts
+++ packages/core/tests/pipe.test.ts
@@ -1,6 +1,16 @@
+import { n } from '@nuxo/core';
 import { assertEquals } from '@std/assert';
+
 import { pipe } from '../src/pipe.ts';
 import { err, ok, unwrap } from '../src/result.ts';
+
+class SomeError extends n.failure(
+  'SomeError',
+  {
+    'en-US': 'Some error',
+    'es-ES': 'Error personalizado',
+  },
+) {}
 
 Deno.test('pipe', () => {
   const result = pipe(
@@ -27,8 +37,8 @@ Deno.test('pipe result strict failure', () => {
   const result = pipe(
     'strict',
     (x: number) => ok(x + 1),
-    () => err('SOME_ERROR'),
+    () => err(new SomeError()),
   )(1);
 
-  assertEquals(unwrap(result), 'SOME_ERROR');
+  assertEquals(unwrap(result), new SomeError());
 });
